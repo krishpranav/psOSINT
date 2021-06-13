8735a0d264f25564fc423151247814649ef3cb87
krishpranav <krisna.pranav@gmail.com>
2021-06-13 15:11:18 +0530

    [+] v1.0 [+]

---
+++ Search-Crtsh.ps1
@@ -0,0 +1,20 @@
+<#
+    .SYNOPSIS
+    Gather Open-Source Intelligence using PowerShell.
+                 
+    .DESCRIPTION
+    Gather Open-Source Intelligence from Certificate Search using PowerShell.
+    .EXAMPLE
+    Search-Crtsh -Query jotugaedorm.com -Wildcard | Format-List
+    issuer_ca_id        : 12922
+    issuer_name         : C=US, ST=TX, L=Houston, O="cPanel, Inc.", CN="cPanel, Inc. Certification Authority"
+    name_value          : cpanel.jotugaedorm.com
+    min_cert_id         : 1179646010
+    min_entry_timestamp : 2/6/19 9:46:22 PM
+    not_before          : 2/6/19 12:00:00 AM
+    not_after           : 5/7/19 11:59:59 PM
+    ...
+    
+    .LINK
+    https://github.com/krishpranav/psOSINT/
+#>
